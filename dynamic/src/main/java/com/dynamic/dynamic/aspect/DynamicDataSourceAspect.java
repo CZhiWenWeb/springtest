09a8e0efb91a37d2dd09c22edfe50995e5887561
CZhiWenWeb <2472723116@qq.com>
2019-07-30 17:42:30 +0800

    ddd

---
+++ dynamic/src/main/java/com/dynamic/dynamic/aspect/DynamicDataSourceAspect.java
@@ -0,0 +1,13 @@
+package com.dynamic.dynamic.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * @Author: czw
+ * @CreateTime: 2019-07-30 17:38
+ * @UpdeteTime: 2019-07-30 17:38
+ * @Description:
+ */
+@Aspect
+public class DynamicDataSourceAspect {
+}
